9f1fcf301465b85cb5946aecc2b19d7575c1897e
nuunzioo <nunzioda00@gmail.com>
2026-09-24 20:20:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('push',e=>{
   let d={};try{d=e.data?e.data.json():{}}catch(_){d={body:e.data?e.data.text():''}}
-  e.waitUntil(self.registration.showNotification(d.title||'Uffa.',{body:d.body||'',tag:d.tag||'uffa',data:{url:self.registration.scope+'app.html'}}));
+  e.waitUntil(self.registration.showNotification(d.title||'Uffa.',{body:d.body||'',tag:d.tag||'uffa',data:{url:self.registration.scope+'app%20finale.html'}}));
 });
 self.addEventListener('notificationclick',e=>{
   e.notification.close();
